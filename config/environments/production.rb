ac962b4241576eda3416525b9d4ff6b57c0fcc04
Atul Ubarhande <atul612@gmail.com>
2025-03-08 12:30:43 +0530

    updated redis url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], expires_in: 1.hour }
+  config.cache_store = :redis_cache_store, { url: ENV['REDISCLOUD_URL'], expires_in: 90.minutes }
+
 
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
