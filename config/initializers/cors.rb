ffcdc94af82cd86cea0dbb6d7d8bdbb5d6e65aa9
Atul Ubarhande <atul612@gmail.com>
2025-06-06 17:56:54 +0530

    updated cors to accept vue application requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://outsider-react-8de83560ff04.herokuapp.com', 'http://localhost:3002', 'http://localhost:3000'] # Updated to match client port
+    origins ['https://outsider-react-8de83560ff04.herokuapp.com', 'http://localhost:3002', 'http://localhost:3000', 'https://outsider-vue-fef499229de9.herokuapp.com'] # Updated to match client port
 
     resource '*',
       headers: :any,
