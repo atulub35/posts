b739e70a9bed874840bda51a70e8de37eb364b2f
Atul Ubarhande <atul612@gmail.com>
2025-10-16 11:21:21 +0530

    handle registration and user onboarding flow, added error handling

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,6 +2,8 @@
 
 class User::RegistrationsController < Devise::RegistrationsController
   layout 'devise'
+  respond_to :html, :json, :turbo_stream
+  protect_from_forgery with: :exception, unless: -> { request.format.json? || request.format.turbo_stream? }
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -14,6 +16,46 @@ class User::RegistrationsController < Devise::RegistrationsController
   # def create
   #   super
   # end
+
+  protected
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      # Handle successful registration
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream { redirect_to root_path }
+        format.json do
+          render json: { 
+            message: 'Registration successful',
+            redirect_path: root_path
+          }, status: :created
+        end
+      end
+    else
+      # Handle validation errors for turbo streams
+      respond_to do |format|
+        format.html { super }
+        format.turbo_stream do
+          error_messages = resource.errors.full_messages
+          if error_messages.any?
+            flash.now[:alert] = error_messages.to_sentence
+          else
+            flash.now[:alert] = "Registration failed. Please try again."
+          end
+          
+          # flash_html = render_to_string(partial: "shared/flash/notification")
+          render turbo_stream: turbo_stream.append("flash-notification", partial: "shared/flash/notification")
+        end
+        format.json do
+          render json: { 
+            errors: resource.errors.full_messages,
+            status: :unprocessable_entity 
+          }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
 
   # GET /resource/edit
   # def edit
